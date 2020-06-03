4c8c6c7b32d4382c5bfc72ed6881cbb99b8489dc
Dobuzi <kimjw7981@gmail.com>
2020-06-03 20:57:01 +0900

    useEffect is applied

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './App.css';
+import { useEffect, useState } from "react";
 
 import useInput from "./useInput/useInput";
 import useTabs from "./useTabs/useTabs";
@@ -9,6 +10,10 @@ export default function App() {
   const noAt = value => !value.includes("@");
   const name = useInput("Mr. ", noAt);
   const { currentItem, changeItem } = useTabs(0, tabs);
+  const [number, setNumber] = useState(0);
+  const [number_2, setNumber_2] = useState(0);
+  const handleNumber = () => { console.log(number) };
+  useEffect(handleNumber, [number, number_2]);
   return (
     <div className="App">
       <h1>Hello</h1>
@@ -16,6 +21,10 @@ export default function App() {
       <div>
         {tabs.map((tab, index) => <button onClick={() => changeItem(index)}>{tab.id}</button>)}
         <h2>{currentItem.content}</h2>
+      </div>
+      <div>
+        <button onClick={() => setNumber(number + 1)}>{number}</button>
+        <button onClick={() => setNumber_2(number_2 + 1)}>{number_2}</button>
       </div>
     </div>
   );
