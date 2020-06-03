8898e7d53150dab2fb0d56cdfd47cb37f74b2b7d
Dobuzi <kimjw7981@gmail.com>
2020-06-03 21:14:40 +0900

    useTitle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 import useInput from "./useInput/useInput";
 import useTabs from "./useTabs/useTabs";
+import useTitle from "./useTitle/index";
 
 export default function App() {
   const noAt = value => !value.includes("@");
@@ -14,12 +15,14 @@ export default function App() {
   const [number_2, setNumber_2] = useState(0);
   const handleNumber = () => { console.log(number) };
   useEffect(handleNumber, [number, number_2]);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 3000);
   return (
     <div className="App">
       <h1>Hello</h1>
       <input placeholder="Name" {...name} />
       <div>
-        {tabs.map((tab, index) => <button onClick={() => changeItem(index)}>{tab.id}</button>)}
+        {tabs.map((tab, index) => <button key={tab.id} onClick={() => changeItem(index)}>{tab.id}</button>)}
         <h2>{currentItem.content}</h2>
       </div>
       <div>
